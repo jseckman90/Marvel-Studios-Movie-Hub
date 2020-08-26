21aef056f7ca14f1701942be6b5833aa5a35368e
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-08-25 21:18:07 -0400

    linked title, plot, and cast

--- app.js
+++ app.js
@@ -28,48 +28,124 @@ const spiderManFarFromHome = '429617'
 
 let posterArray = [ironMan, hulk, ironMan2, thor, capAmerica, avengers, ironMan3, thorDarkWorld,capAmericaWS, guardians, avengersUltron, antMan,capAmericaCW, doctorStrange,guardians2, spiderManHomecoming, thorRag, blackPanther, avengersInfinity, antManWasp,captainMarvel, avengersEndgame, spiderManFarFromHome]
 
+
+
 for (i = 0; i < posterArray.length; i++) {
   let movie = posterArray[i]
-$.ajax({
+
+
   
+$.ajax({
 
   url: `https://api.themoviedb.org/3/movie/${movie}?api_key=c153d46ebc986f5c9274a30a6c3111e8&language=en-US`,
   }).then((data) => {
-    console.log(data);
-    // $('#info').append($('<div>').text(data.title))
+    // console.log(data);
+    
+    $('#movie-info').append($(`<div id = ${movie}-info>`).append(`<h1 class = "title">${data.title}</h1>`).css('display','none'))
+    $(`#${movie}-info`).append(`<h4 class = "plot">${data.overview}</h4>`)
+    $(`#${movie}-info`).append('<ul class="characters">')
+
+
+    $.ajax({
+      url: `https://api.themoviedb.org/3/movie/${movie}/credits?api_key=c153d46ebc986f5c9274a30a6c3111e8`,
+      }).then((data) => {
+          // console.log(data.cast);
+          for(i = 0; i<data.cast.length; i++) {
+            $(`#${movie}-info`).children('.characters').append(`<li>${data.cast[i].character}</li>`)
+         
+          }
+    
+          
+          
+        });
 
 
     $.ajax({
 
       url: `http://www.omdbapi.com/?apikey=aa940672&t=${data.title}`,
       }).then((data) => {
-          console.log(data);
+          // console.log(data);
+          $('.carousel-images').append(`<img id = "${movie}" class="poster" src="${data.Poster}">`)
+          
     
           
-          $('#poster-carousel').append(`<img class="poster" src="${data.Poster}">`)
-    
+          
+ ///////////////////////////////////
+ //CLICK ON POSTER TO GET MOVIE DETAILS/CAST
+ ///////////////////////////////////
+
+
           $('.poster').on('click', (event) => {
             event.preventDefault();
+            
+            console.log(event.currentTarget.id)
+            
           
             $.ajax({
               url: `https://api.themoviedb.org/3/movie/${movie}/credits?api_key=c153d46ebc986f5c9274a30a6c3111e8`,
               }).then((data) => {
-                console.log(data.cast);
-            
-                for (i = 0; i< data.cast.length; i++) {
-                  $('#info').append($('<div class = "character">').text(data.cast[i].character))
-                }
-            
-            });
-            
-            
-          })
+                  // console.log(data.cast);
+                  if ($(`#${event.currentTarget.id}-info`).css('display','none')) {
+                    $(`#${event.currentTarget.id}-info`).css('display','block')
+                  } else {
+                    $(`#${event.currentTarget.id}-info`).css('display','none')
+                  }
+                  
           
-                
-    });
             
-});
-}           
+                  
+                  
+                });
+            })
+              
+                    
+      });
+                
+  });
+}      
+                
+                 
+                
+            
+            
+            
+
+
+
+
+////////////////////////////////////////////////
+//////next button and previous button
+////////////////////////////////////////////////
+
+// let posterIndex = 0
+// let highestIndex = $(".carousel-images").children().length - 1;
+
+// $(".carousel-button-next").on("click", () => {
+            
+//   console.log("Next was clicked");
+//   $(".carousel-images").children().eq(posterIndex).css("display", "none");
+  
+//   if (posterIndex < highestIndex) {
+//     posterIndex++;
+//   } else {
+//     posterIndex = 0;
+//   }
+//   $(".carousel-images").children().eq(posterIndex).css("display", "block");
+// });
+          
+// $(".carousel-button-previous").on("click", () => {
+            
+//   console.log("Previous was clicked");
+//   $(".carousel-images").children().eq(posterIndex).css("display", "none");
+  
+//   if (posterIndex > 0) {
+//     posterIndex--;
+//   } else {
+//     posterIndex = highestIndex;
+//   }
+//   $(".carousel-images").children().eq(posterIndex).css("display", "block");
+// });
+
           
 
 
