9fc1f51b703c7411a65fcb3b1d14ba415402e919
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-08-26 21:27:09 -0400

    carousel now functional

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const ironMan = '1726'
 const hulk = '1724'
 const ironMan2 = '10138'
@@ -28,24 +26,25 @@ const spiderManFarFromHome = '429617'
 
 let posterArray = [ironMan, hulk, ironMan2, thor, capAmerica, avengers, ironMan3, thorDarkWorld,capAmericaWS, guardians, avengersUltron, antMan,capAmericaCW, doctorStrange,guardians2, spiderManHomecoming, thorRag, blackPanther, avengersInfinity, antManWasp,captainMarvel, avengersEndgame, spiderManFarFromHome]
 
-
+let highestIndex = 0
 
 for (i = 0; i < posterArray.length; i++) {
   let movie = posterArray[i]
-
-
-  
+  //////////////////////////////////
+  // Append movie info to movie-info div
+  ///////////////////////////////////
 $.ajax({
-
   url: `https://api.themoviedb.org/3/movie/${movie}?api_key=c153d46ebc986f5c9274a30a6c3111e8&language=en-US`,
   }).then((data) => {
     // console.log(data);
-    
     $('#movie-info').append($(`<div id = ${movie}-info>`).append(`<h1 class = "title">${data.title}</h1>`).css('display','none'))
-    $(`#${movie}-info`).append(`<h4 class = "plot">${data.overview}</h4>`)
+    $(`#${movie}-info`).append(`<h2 class = "year">Released: ${data.release_date}</h2>`)
+    $(`#${movie}-info`).append(`<h3 class = "plot">${data.overview}</h3>`)
     $(`#${movie}-info`).append('<ul class="characters">')
 
-
+    ///////////////////////////
+    // Append cast to movie-info div
+    ///////////////////////////
     $.ajax({
       url: `https://api.themoviedb.org/3/movie/${movie}/credits?api_key=c153d46ebc986f5c9274a30a6c3111e8`,
       }).then((data) => {
@@ -54,102 +53,72 @@ $.ajax({
             $(`#${movie}-info`).children('.characters').append(`<li>${data.cast[i].character}</li>`)
          
           }
-    
-          
-          
         });
-
-
+    
+    //////////////////////////////////////////////////////
+    // Append posters to caorusel-images in the poster-carousel div
+    //////////////////////////////////////////////////////
     $.ajax({
-
       url: `http://www.omdbapi.com/?apikey=aa940672&t=${data.title}`,
       }).then((data) => {
           // console.log(data);
           $('.carousel-images').append(`<img id = "${movie}" class="poster" src="${data.Poster}">`)
-          
-    
-          
-          
- ///////////////////////////////////
- //CLICK ON POSTER TO GET MOVIE DETAILS/CAST
- ///////////////////////////////////
 
+          ///////////////////////////////////
+          //CLICK ON POSTER TO GET MOVIE DETAILS/CAST
+          ///////////////////////////////////
 
-          $('.poster').on('click', (event) => {
+          
+          $(`#${movie}`).on('click', (event) => {
             event.preventDefault();
-            
-            console.log(event.currentTarget.id)
-            
+            console.log(event.currentTarget)
+             $(`#${event.currentTarget.id}-info`).toggle()
+          })
+
+          highestIndex = $(".carousel-images").children().length - 1;
+
           
-            $.ajax({
-              url: `https://api.themoviedb.org/3/movie/${movie}/credits?api_key=c153d46ebc986f5c9274a30a6c3111e8`,
-              }).then((data) => {
-                  // console.log(data.cast);
-                  if ($(`#${event.currentTarget.id}-info`).css('display','none')) {
-                    $(`#${event.currentTarget.id}-info`).css('display','block')
-                  } else {
-                    $(`#${event.currentTarget.id}-info`).css('display','none')
-                  }
-                  
-          
-            
-                  
-                  
-                });
-            })
-              
-                    
-      });
-                
+ 
+        });
   });
 }      
-                
-                 
-                
+
+        
+          
             
+        
+          
+let posterIndex = 0
+
+
+
+
+$(".next").on("click", () => {
             
-            
-
-
-
-
-////////////////////////////////////////////////
-//////next button and previous button
-////////////////////////////////////////////////
-
-// let posterIndex = 0
-// let highestIndex = $(".carousel-images").children().length - 1;
-
-// $(".carousel-button-next").on("click", () => {
-            
-//   console.log("Next was clicked");
-//   $(".carousel-images").children().eq(posterIndex).css("display", "none");
+  console.log("Next was clicked") ;
   
-//   if (posterIndex < highestIndex) {
-//     posterIndex++;
-//   } else {
-//     posterIndex = 0;
-//   }
-//   $(".carousel-images").children().eq(posterIndex).css("display", "block");
-// });
-          
-// $(".carousel-button-previous").on("click", () => {
-            
-//   console.log("Previous was clicked");
-//   $(".carousel-images").children().eq(posterIndex).css("display", "none");
+  $(".carousel-images").children().eq(posterIndex).css("display", "none");
   
-//   if (posterIndex > 0) {
-//     posterIndex--;
-//   } else {
-//     posterIndex = highestIndex;
-//   }
-//   $(".carousel-images").children().eq(posterIndex).css("display", "block");
-// });
-
+  if (posterIndex < highestIndex) {
+    posterIndex++;
+  } else {
+    posterIndex = 0;
+  }
+  $(".carousel-images").children().eq(posterIndex).css("display", "block");
+});
           
-
-
-
-          
-
+$(".previous").on("click", () => {
+            
+  console.log("Previous was clicked");
+  $(".carousel-images").children().eq(posterIndex).css("display", "none");
+  
+  if (posterIndex > 0) {
+    posterIndex--;
+  } else {
+    posterIndex = highestIndex;
+  }
+  $(".carousel-images").children().eq(posterIndex).css("display", "block");
+});       
+              
+                    
 
