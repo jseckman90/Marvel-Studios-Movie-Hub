45f1fc8a9ca230df4a27d6437c0cd901aa9a46e7
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-08-27 20:47:12 -0400

    added css styling, phase titles, new header logo

--- app.js
+++ app.js
@@ -1,57 +1,170 @@
 //PHASE 1
-const ironMan = '1726'
-const hulk = '1724'
-const ironMan2 = '10138'
-const thor = '10195'
-const capAmerica = '1771'
-const avengers = '24428'
+const ironMan = {
+  id: '1726',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg',
+  phase: '1',
+}
+const hulk = {
+  id: '1724',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTUyNzk3MjA1OF5BMl5BanBnXkFtZTcwMTE1Njg2MQ@@._V1_SX300.jpg',
+  phase: '1'
+}
+const ironMan2 = {
+  id: '10138',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_SX300.jpg',
+  phase: '1'
+}
+const thor = {
+  id: '10195',
+  poster: 'https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+  phase: '1'
+}
+const capAmerica = {
+  id: '1771',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_SX300.jpg',
+  phase: '1'
+}
+const avengers = {
+  id: '24428',
+  poster: 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  phase: '1'
+}
 //PHASE 2
-const ironMan3 = '68721'
-const thorDarkWorld = '76338'
-const capAmericaWS = '100402'
-const guardians = '118340'
-const avengersUltron = '99861'
-const antMan = '102899'
+const ironMan3 = {
+  id: '68721',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg',
+  phase: '2'
+}
+const thorDarkWorld = {
+  id: '76338',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTQyNzAwOTUxOF5BMl5BanBnXkFtZTcwMTE0OTc5OQ@@._V1_SX300.jpg',
+  phase: '2'
+}
+const capAmericaWS = {
+  id: '100402',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMzA2NDkwODAwM15BMl5BanBnXkFtZTgwODk5MTgzMTE@._V1_SX300.jpg',
+  phase: '2'
+}
+const guardians = {
+  id: '118340',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_SX300.jpg',
+  phase: '2'
+}
+const avengersUltron = {
+  id: '99861',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
+  phase: '2'
+}
+const antMan = {
+  id: '102899',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_SX300.jpg',
+  phase: '2'
+}
 //PHASE 3
-const capAmericaCW = '271110'
-const doctorStrange = '284052'
-const guardians2 = '283995'
-const spiderManHomecoming = '315635'
-const thorRag = '284053'
-const blackPanther = '284054'
-const avengersInfinity = '299536'
-const antManWasp = '363088'
-const captainMarvel = '299537'
-const avengersEndgame = '299534'
-const spiderManFarFromHome = '429617'
+const capAmericaCW = {
+  id: '271110',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg',
+  phase: '3'
+}
+const doctorStrange = {
+  id: '284052',
+  poster: 'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg',
+  phase: '3'
+}
+const guardians2 = {
+  id: '283995',
+  poster: 'https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg',
+  phase: '3'
+}
+const spiderManHomecoming = {
+  id: '315635',
+  poster: 'https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SX300.jpg',
+  phase: '3'
+}
+const thorRag = {
+  id: '284053',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@._V1_SX300.jpg',
+  phase: '3'
+}
+const blackPanther = {
+  id: '284054',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_SX300.jpg',
+  phase: '3'
+}
+const avengersInfinity = {
+  id: '299536',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+  phase: '3'
+}
+const antManWasp = {
+  id: '363088',
+  poster: 'https://m.media-amazon.com/images/M/MV5BYjcyYTk0N2YtMzc4ZC00Y2E0LWFkNDgtNjE1MzZmMGE1YjY1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  phase: '3'
+}
+const captainMarvel = {
+  id: '299537',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_SX300.jpg',
+  phase: '3'
+}
+const avengersEndgame = {
+  id: '299534',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
+  phase: '3'
+}
+const spiderManFarFromHome = {
+  id: '429617',
+  poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg',
+  phase: '3'
+}
 
-let posterArray = [ironMan, hulk, ironMan2, thor, capAmerica, avengers, ironMan3, thorDarkWorld,capAmericaWS, guardians, avengersUltron, antMan,capAmericaCW, doctorStrange,guardians2, spiderManHomecoming, thorRag, blackPanther, avengersInfinity, antManWasp,captainMarvel, avengersEndgame, spiderManFarFromHome]
+let movieArray = [ironMan, hulk, ironMan2, thor, capAmerica, avengers, ironMan3, thorDarkWorld,capAmericaWS, guardians, avengersUltron, antMan,capAmericaCW, doctorStrange,guardians2, spiderManHomecoming, thorRag, blackPanther, avengersInfinity, antManWasp,captainMarvel, avengersEndgame, spiderManFarFromHome]
+
+
 
 let highestIndex = 0
+let highestTitleIndex = $("#phase-title").children().length - 1;
 
-for (i = 0; i < posterArray.length; i++) {
-  let movie = posterArray[i]
+
+
+
+for (i = 0; i < movieArray.length; i++) {
+  let movie = movieArray[i]
+ console.log(movie.id)
+  if (movie.phase === '1') {
+  $('#phase-1').append(`<img id = "${movie.id}" class="poster" src="${movie.poster}">`)
+  }
+
+  if (movie.phase === '2') {
+    $('#phase-2').append(`<img id = "${movie.id}" class="poster" src="${movie.poster}">`)
+  }
+
+  if (movie.phase === '3') {
+    $('#phase-3').append(`<img id = "${movie.id}" class="poster" src="${movie.poster}">`)
+  }
+
+
+
   //////////////////////////////////
   // Append movie info to movie-info div
   ///////////////////////////////////
 $.ajax({
-  url: `https://api.themoviedb.org/3/movie/${movie}?api_key=c153d46ebc986f5c9274a30a6c3111e8&language=en-US`,
+  url: `https://api.themoviedb.org/3/movie/${movie.id}?api_key=c153d46ebc986f5c9274a30a6c3111e8&language=en-US`,
   }).then((data) => {
     // console.log(data);
-    $('#movie-info').append($(`<div id = ${movie}-info class="movie-info">`).append(`<h1 class = "title">${data.title}</h1>`).css('display','none'))
-    $(`#${movie}-info`).append(`<h2 class = "year">Released: ${data.release_date}</h2>`)
-    $(`#${movie}-info`).append(`<h3 class = "plot">${data.overview}</h3>`)
-    $(`#${movie}-info`).append('<ul class="characters">')
+    $('#movie-info').append($(`<div id = ${movie.id}-info class="movie-info">`).append(`<h1 class = "title">${data.title}</h1>`).css('display','none'))
+    $(`#${movie.id}-info`).append(`<h2 class = "year">Released: ${data.release_date}</h2>`)
+    $(`#${movie.id}-info`).append(`<h3 class = "plot">${data.overview}</h3>`)
+    $(`#${movie.id}-info`).append('<ul class="characters">')
 
     ///////////////////////////
     // Append cast to movie-info div
     ///////////////////////////
     $.ajax({
-      url: `https://api.themoviedb.org/3/movie/${movie}/credits?api_key=c153d46ebc986f5c9274a30a6c3111e8`,
+      url: `https://api.themoviedb.org/3/movie/${movie.id}/credits?api_key=c153d46ebc986f5c9274a30a6c3111e8`,
       }).then((data) => {
           // console.log(data.cast);
           for(i = 0; i<data.cast.length; i++) {
-            $(`#${movie}-info`).children('.characters').append(`<li>${data.cast[i].character}</li>`)
+            $(`#${movie.id}-info`).children('.characters').append(`<li>${data.cast[i].character}</li>`)
          
           }
         });
@@ -59,18 +172,15 @@ $.ajax({
     //////////////////////////////////////////////////////
     // Append posters to caorusel-images in the poster-carousel div
     //////////////////////////////////////////////////////
-    $.ajax({
-      url: `http://www.omdbapi.com/?apikey=aa940672&t=${data.title}`,
-      }).then((data) => {
-          // console.log(data);
-          $('.carousel-images').append(`<img id = "${movie}" class="poster" src="${data.Poster}">`)
-
+   
+  
+          
           ///////////////////////////////////
           //CLICK ON POSTER TO GET MOVIE DETAILS/CAST
           ///////////////////////////////////
 
           
-          $(`#${movie}`).on('click', (event) => {
+          $(`#${movie.id}`).on('click', (event) => {
             event.preventDefault();
             console.log(event.currentTarget)
              $(`#${event.currentTarget.id}-info`).toggle()
@@ -80,18 +190,13 @@ $.ajax({
 
           
  
-        });
+        
   });
 }      
-
-        
-          
-            
-        
+       
           
 let posterIndex = 0
-
-
+let titleIndex = 0
 
 
 $(".next").on("click", () => {
@@ -105,7 +210,16 @@ $(".next").on("click", () => {
   } else {
     posterIndex = 0;
   }
-  $(".carousel-images").children().eq(posterIndex).css("display", "block");
+  $(".carousel-images").children().eq(posterIndex).css("display", "flex");
+
+
+  $('#phase-title').children(titleIndex).css('display', "none")
+  if (titleIndex < highestTitleIndex) {
+    titleIndex++;
+  } else {
+    titleIndex = 0;
+  }
+  $("#phase-title").children().eq(titleIndex).css("display", "block");
   $(`.movie-info`).css('display', 'none')
 });
           
@@ -119,7 +233,18 @@ $(".previous").on("click", () => {
   } else {
     posterIndex = highestIndex;
   }
-  $(".carousel-images").children().eq(posterIndex).css("display", "block");
+
+  $('#phase-title').children(titleIndex).css('display', "none")
+  if (titleIndex < highestTitleIndex) {
+    titleIndex--;
+  } else {
+    titleIndex = highestTitleIndex;
+  }
+  $("#phase-title").children().eq(titleIndex).css("display", "block");
+  $(`.movie-info`).css('display', 'none')
+  
+  $(".carousel-images").children().eq(posterIndex).css("display", "flex");
+  $("#phase-title").children().eq(titleIndex).css("display", "block");
 });       
               
                     
